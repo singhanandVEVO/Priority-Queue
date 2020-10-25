efdae3781f32d88e04ea8c4cb197989d16b6d52d
singhanandVEVO <singhanand037@gmail.com>
2020-10-25 19:57:26 +0530

    Implemented printSortedSquaredArray method

--- c++ code/squareSortedArray.cpp
+++ c++ code/squareSortedArray.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 
 void printSortedSquaredArray(int A[],int numOfElements) {
-    // todo 
+    int left = 0, right = numOfElements -1;
+    int sortedArray[numOfElements];
+    for(int i = numOfElements - 1; i>=0; i--) {
+        if(abs(A[left]) > abs(A[right])) {
+            sortedArray[i] = A[left] * A[left];
+            left++;
+        } else {
+            sortedArray[i] = A[right] * A[right];
+            right--;
+        }
+    }
+    for(int i=0; i<numOfElements; i++) {
+        cout<<""<<sortedArray[i]<<" ";
+    }
+    cout<<endl;
 }
 
 
