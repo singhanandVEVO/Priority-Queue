6605249da4be8a717c8582dfd9059a45701fe3ee
singhanandVEVO <singhanand037@gmail.com>
2020-10-25 20:20:18 +0530

    Added getReverseWordStringExcludeSpace method passing all leetcode test cases for given problem

--- c++ code/reverseWordsInString.cpp
+++ c++ code/reverseWordsInString.cpp
@@ -24,6 +24,39 @@ string getReversedWordString(string originalString) {
     return reversedString;
 }
 
+// leetcode problem solution 
+string getReverseWordStringExcludeSpace( string s) {
+    string reversedString = "", tempWordString = "";
+    const char Blankspace = ' ';
+    int i,size = s.size(), count = 0;
+
+    for(i=size-1; i>=0; i--) {
+
+        if(s[i] == Blankspace) {
+            if(tempWordString == "") continue;
+            count++;
+            if(count > 1) {
+                reversedString +=  " " + tempWordString;
+            } else {
+                reversedString += tempWordString;
+                
+            }
+            tempWordString = "";
+
+        } else {
+            tempWordString = s[i] + tempWordString;
+        };
+    }
+    if(tempWordString.size()>0 && count > 1) {
+        reversedString += " " + tempWordString;
+    } else  {
+        reversedString += tempWordString;
+    }
+   
+    return reversedString;
+        
+}
+
 int main() {
     string str = "i works as a software engineer";
     string reversedWordString  = getReversedWordString(str);
